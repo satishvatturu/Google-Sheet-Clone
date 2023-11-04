531986e8c6db11f5f1a1ee1eff08920fa342c491
Satish Vatturu <vatturu.srinagasatish@gmail.com>
2023-11-04 18:12:23 +0530

    Completed the UI and added the CellID funtionality

---
+++ script.js
@@ -0,0 +1,50 @@
+let rows = 100;
+let cols = 26;
+
+let addressColCont = document.querySelector(".address-col-cont");
+let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar")
+
+// For creating 1 - 100 rows
+for(let i=0;i<rows;i++)
+{
+    let addressCol = document.createElement("div");
+    addressCol.setAttribute("class", "address-col");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
+}
+
+// for creating A - Z cols
+for(let i=0;i<cols;i++)
+{
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class", "address-row");
+    addressRow.innerText = String.fromCharCode(65+i);
+    addressRowCont.appendChild(addressRow); 
+}
+
+// for creating cells
+for(let i=0;i<rows;i++)
+{
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class", "rowCont");
+    for(let j=0;j<cols;j++)
+    {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        rowCont.appendChild(cell);
+        addListenerForAddressBarDisplay(cell, i, j);
+    }
+    cellsCont.appendChild(rowCont);
+}
+
+// for displaying cellID in address-bar.
+function addListenerForAddressBarDisplay(cell, i, j) {
+    cell.addEventListener("click", (e)=>{
+        let rowID = i+1;
+        let colID = String.fromCharCode(65+j);
+        addressBar.value = `${colID}${rowID}`
+    })
+}
